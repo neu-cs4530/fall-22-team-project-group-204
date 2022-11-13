4a75bb6b031c44549ff7f0e65c84cc0dd321a5be
Jessica Yu <jessicayu62@gmail.com>
2022-11-13 01:34:59 -0500

    put data into array for leaderboard table

--- frontend/src/components/Leaderboard/LeaderboardModal.tsx
+++ frontend/src/components/Leaderboard/LeaderboardModal.tsx
@@ -14,10 +14,82 @@ import {
   Thead,
   Tr,
 } from '@chakra-ui/react';
+import { createColumnHelper } from '@tanstack/react-table';
 import React, { useEffect, useState } from 'react';
 import useTownController from '../../hooks/useTownController';
+import { DataTable } from './DataTable';
 
 export default function LeaderboardModal(): JSX.Element {
+  type PlayerStanding = {
+    ranking: number;
+    name: string;
+    wins: number;
+    reward: number;
+  };
+
+  const data: PlayerStanding[] = [
+    {
+      ranking: 1,
+      name: 'Player1',
+      wins: 6,
+      reward: 400,
+    },
+    {
+      ranking: 2,
+      name: 'Player2',
+      wins: 3,
+      reward: 500,
+    },
+    {
+      ranking: 3,
+      name: 'Player3',
+      wins: 2,
+      reward: 320,
+    },
+    {
+      ranking: 4,
+      name: 'Player4',
+      wins: 2,
+      reward: 320,
+    },
+    {
+      ranking: 5,
+      name: 'Player5',
+      wins: 1,
+      reward: 140,
+    },
+    {
+      ranking: 6,
+      name: 'Player6',
+      wins: 1,
+      reward: 140,
+    },
+    {
+      ranking: 7,
+      name: 'Player7',
+      wins: 1,
+      reward: 140,
+    },
+    {
+      ranking: 8,
+      name: 'Player8',
+      wins: 1,
+      reward: 140,
+    },
+    {
+      ranking: 9,
+      name: 'Player9',
+      wins: 0,
+      reward: 0,
+    },
+    {
+      ranking: 10,
+      name: 'Player10',
+      wins: 0,
+      reward: 0,
+    },
+  ];
+
   const coveyTownController = useTownController();
   // State for keeping track of whether key is pressed
   const [showModal, setShowModal] = useState<boolean>(false);
@@ -46,6 +118,36 @@ export default function LeaderboardModal(): JSX.Element {
     };
   });
 
+  const columnHelper = createColumnHelper<PlayerStanding>();
+
+  const columns = [
+    columnHelper.accessor('ranking', {
+      cell: info => info.getValue(),
+      header: 'Ranking',
+      meta: {
+        isNumeric: true,
+      },
+    }),
+    columnHelper.accessor('name', {
+      cell: info => info.getValue(),
+      header: 'Name',
+    }),
+    columnHelper.accessor('wins', {
+      cell: info => info.getValue(),
+      header: 'Wins',
+      meta: {
+        isNumeric: true,
+      },
+    }),
+    columnHelper.accessor('reward', {
+      cell: info => info.getValue(),
+      header: 'Reward',
+      meta: {
+        isNumeric: true,
+      },
+    }),
+  ];
+
   return (
     <Modal
       size='xl'
@@ -54,79 +156,9 @@ export default function LeaderboardModal(): JSX.Element {
         closeModal();
       }}
       scrollBehavior='inside'>
-      <ModalOverlay />
       <ModalContent>
         <ModalHeader>Blackjack Leaderboard</ModalHeader>
-        <TableContainer overflowY='scroll'>
-          <Table variant='simple' size='lg'>
-            <Thead>
-              <Tr>
-                <Th isNumeric>Ranking</Th>
-                <Th>Name</Th>
-                <Th isNumeric>Wins</Th>
-                <Th isNumeric>Rewards</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td isNumeric>1</Td>
-                <Td>Player1</Td>
-                <Td isNumeric>6</Td>
-                <Td isNumeric>400</Td>
-              </Tr>
-              <Tr>
-                <Td isNumeric>2</Td>
-                <Td>Player2</Td>
-                <Td isNumeric>3</Td>
-                <Td isNumeric>500</Td>
-              </Tr>
-              <Tr>
-                <Td isNumeric>3</Td>
-                <Td>Player3</Td>
-                <Td isNumeric>2</Td>
-                <Td isNumeric>320</Td>
-              </Tr>
-              <Tr>
-                <Td isNumeric>4</Td>
-                <Td>Player4</Td>
-                <Td isNumeric>2</Td>
-                <Td isNumeric>320</Td>
-              </Tr>
-              <Tr>
-                <Td isNumeric>5</Td>
-                <Td>Player5</Td>
-                <Td isNumeric>1</Td>
-                <Td isNumeric>140</Td>
-              </Tr>
-              <Tr>
-                <Td isNumeric>6</Td>
-                <Td>Player6</Td>
-                <Td isNumeric>1</Td>
-                <Td isNumeric>140</Td>
-              </Tr>
-              <Tr>
-                <Td isNumeric>7</Td>
-                <Td>Player7</Td>
-                <Td isNumeric>1</Td>
-                <Td isNumeric>140</Td>
-              </Tr>
-              <Tr>
-                <Td isNumeric>8</Td>
-                <Td>Player8</Td>
-                <Td isNumeric>1</Td>
-                <Td isNumeric>140</Td>
-              </Tr>
-            </Tbody>
-            <Tfoot>
-              <Tr>
-                <Th isNumeric>Ranking</Th>
-                <Th>Name</Th>
-                <Th isNumeric>Wins</Th>
-                <Th isNumeric>Rewards</Th>
-              </Tr>
-            </Tfoot>
-          </Table>
-        </TableContainer>
+        <DataTable columns={columns} data={data} />
         <ModalFooter>
           <Button colorScheme='blue' mr={3} onClick={closeModal}>
             Done
