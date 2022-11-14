13f0a49eaea90b0f65852e7ad392c8c917a987fe
zcolucci1012 <32983507+zcolucci1012@users.noreply.github.com>
2022-11-13 19:20:53 -0500

    Started work on front end implementation: - Added a background image - Added a couple of sample cards

--- frontend/src/components/Town/interactables/BlackjackModal.tsx
+++ frontend/src/components/Town/interactables/BlackjackModal.tsx
@@ -1,15 +1,87 @@
 import {
   Button,
   Modal,
+  ModalBody,
   ModalCloseButton,
   ModalContent,
   ModalFooter,
   ModalHeader,
   ModalOverlay,
+  Image,
+  Container,
+  Box,
 } from '@chakra-ui/react';
+import { CardMedia } from '@material-ui/core';
+import { identity } from 'lodash';
 import React, { useCallback, useEffect } from 'react';
 import { useInteractable } from '../../../classes/TownController';
 import useTownController from '../../../hooks/useTownController';
+
+// i need to figure out how the back and front end communicate :C
+
+// import Value, { getValueNumbers } from '../../../../../townService/src/games/cards/Value';
+// import Suit from '../../../../../townService/src/games/cards/Suit';
+
+// export function Card({ value, suit }: { value: Value; suit: Suit }) {
+//   let cardValueId: number;
+//   switch (value) {
+//     case Value.Ace:
+//       cardValueId = 13;
+//       break;
+//     case Value.King:
+//       cardValueId = 12;
+//       break;
+//     case Value.Queen:
+//       cardValueId = 11;
+//       break;
+//     case Value.Jack:
+//       cardValueId = 10;
+//       break;
+//     default:
+//       cardValueId = getValueNumbers(value)[0] - 1;
+//       break;
+//   }
+//   let cardId: number;
+//   switch (suit) {
+//     case Suit.Spades:
+//       cardId = cardValueId;
+//       break;
+//     case Suit.Clubs:
+//       cardId = cardValueId + 13;
+//       break;
+//     case Suit.Diamonds:
+//       cardId = cardValueId + 26;
+//       break;
+//     case Suit.Hearts:
+//       cardId = cardValueId + 39;
+//       break;
+//   }
+//   const cardIdString = ('0' + cardId).slice(-2);
+//   const card = `assets/blackjack/playingcards/playingcards_${cardIdString}.png`;
+//   return <Image src={card} />;
+// }
+
+export function Card({ cardId, x, y }: { cardId: number; x: number; y: number }) {
+  const cardIdString = ('0' + cardId).slice(-2);
+  const card = `assets/blackjack/playingcards/playingcards_${cardIdString}.png`;
+  return <Image width='64px' position='absolute' src={card} top={y + 'px'} left={x + 'px'} />;
+}
+
+export function Blackjack() {
+  return (
+    <Box
+      backgroundImage='assets/blackjack/blackjack_background.jpg'
+      width='960px'
+      height='600px'
+      backgroundRepeat='no-repeat'
+      backgroundSize='cover'
+      position='relative'>
+      <Card cardId={33} x={450} y={400} />
+      <Card cardId={22} x={475} y={425} />
+      {/*<Image src='assets/blackjack/blackjack_background.jpg' boxSize='100%' objectFit='cover' />*/}
+    </Box>
+  );
+}
 
 export default function BlackjackModal(): JSX.Element {
   const coveyTownController = useTownController();
@@ -37,10 +109,14 @@ export default function BlackjackModal(): JSX.Element {
       onClose={() => {
         closeModal();
         coveyTownController.unPause();
-      }}>
+      }}
+      size='5xl'>
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader>BLACKJACK PLACEHOLDER</ModalHeader>
+        <ModalHeader>Blackjack!</ModalHeader>
+        <ModalBody>
+          <Blackjack />
+        </ModalBody>
         <ModalCloseButton />
         <ModalFooter>
           <Button colorScheme='blue' mr={3} onClick={closeModal}>
