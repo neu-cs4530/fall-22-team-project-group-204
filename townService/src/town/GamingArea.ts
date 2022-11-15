28f5740c436d6551b0b9b951d007ad233ad4de28
zcolucci1012 <32983507+zcolucci1012@users.noreply.github.com>
2022-11-15 16:16:26 -0500

    Built out methods for updateFromBlackjack - Made it easier to convert from Blackjack's data types to GamingArea's   data types - (Should likely be moved into new branch)

--- townService/src/town/GamingArea.ts
+++ townService/src/town/GamingArea.ts
@@ -8,8 +8,9 @@ import {
 } from '../types/CoveyTownSocket';
 import InteractableArea from './InteractableArea';
 import BlackJack from '../games/blackjack/blackjack/Blackjack';
-// import DealerPlayer from '../games/blackjack/players/DealerPlayer';
+import DealerPlayer from '../games/blackjack/players/DealerPlayer';
 import HumanPlayer from '../games/blackjack/players/HumanPlayer';
+import Hand from '../games/blackjack/players/Hand';
 
 export default class GamingArea extends InteractableArea {
   private _dealerHand: PlayingCard[];
@@ -69,11 +70,38 @@ export default class GamingArea extends InteractableArea {
    *
    * @param gamingArea
    */
-  // public updateFromBlackjack(dealer: DealerPlayer, players: HumanPlayer[]) {
-  //   this._dealerHand = dealer.hand;
-  //   this._playerHands = playerHands;
-  //   this._emitAreaChanged();
-  // }
+  public updateFromBlackjack(dealer: DealerPlayer, players: HumanPlayer[]) {
+    this._dealerHand = GamingArea.handToListOfPlayingCards(dealer.hand);
+    this._playerHands = GamingArea.playersToPlayerHands(players);
+    this._emitAreaChanged();
+  }
+
+  /**
+   * Static method that helps convert a dealer's hand from Blackjack into a usable format
+   *
+   * @param hand The dealer's hand
+   * @returns a usable list of PlayingCards
+   */
+  public static handToListOfPlayingCards(hand: Hand): PlayingCard[] {
+    const playingCards: PlayingCard[] = [];
+    hand.cards.forEach(card => {
+      playingCards.push({ suit: card[0].suit.toString(), value: card[0].value.toString() });
+    });
+    return playingCards;
+  }
+
+  /**
+   * Static method that help converts each player's hand from Blackjack into a usable format
+   * @param players The players
+   * @returns a list of PlayerHands for each player
+   */
+  public static playersToPlayerHands(players: HumanPlayer[]): PlayerHand[] {
+    const playerHands: PlayerHand[] = [];
+    players.forEach(player => {
+      playerHands.push({ hand: GamingArea.handToListOfPlayingCards(player.hand), id: player.id });
+    });
+    return playerHands;
+  }
 
   /**
    * Convert this GamingArea instance to a simple GamingAreaModel suitable for
